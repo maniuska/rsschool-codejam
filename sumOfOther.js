b1b07e8db0230c43a7ee11eaa45ac9737ca41d8b
maniuska <36560859+maniuska@users.noreply.github.com>
2018-10-28 13:05:06 +0300

    Add content of the method sumOfOther

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,3 +1,8 @@
 function sumOfOther(arr){
-
+    var sum = 0;
+    for (var i= 0; i<arr.length; i++)
+        sum+=arr[i];
+        for (var i= 0; i<arr.length; i++)
+            arr[i]= sum-arr[i];
+    return arr; 
 }
