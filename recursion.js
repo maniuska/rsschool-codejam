4d14f7528a7b0176781d0a6c763162f838b679c1
maniuska <36560859+maniuska@users.noreply.github.com>
2018-10-28 17:08:51 +0300

    feat: add recursion method content

--- recursion.js
+++ recursion.js
@@ -1,3 +1,16 @@
-function recursion(arg) {
-
+function recursion (arg, array, index) {
+    array = array || [];
+    index = index || 0;
+    
+    if((array.length <= index) && (arg.value)) 
+        array.push([]);
+    
+    array[index].push(arg.value);
+    
+    if (arg.left) 
+        recursion(arg.left, array, index+1);
+    if (arg.right)  
+        recursion (arg.right, array, index+1);
+    
+    return array;
 }
